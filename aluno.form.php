c6081c1d4828162ffe5db0814dd676ddf62ffe9a
kmitiko <kmitiko1@gmail.com>
2022-03-07 00:34:14 -0300

    new form

---
+++ aluno.form.php
@@ -0,0 +1,30 @@
+<html lang='pt-br'>
+<head>
+
+<meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>Formulario de Cadastro de Alunos </title>
+</head>
+<body>
+    <div class="container">
+        <fieldset>
+            <legend>Cadastro de Alunos</legend>
+            <form action="">
+                <div class="form-group">
+                    <label for="aluno_id">Nome do Aluno</label>
+                    <input class="form-control" type="text" name="txtAluno" id="aluno_id"  placeholder="Informe o nome do Aluno">
+                </div>
+                <div class="form-group">
+                    <label for="email_id">Email do Aluno</label>
+                    <input class="form-control" type="text" name="txtEmail" id="email_id"  placeholder="Informe o Email">
+                </div>
+                <button class="btn btn-dark my-2" type="submit">Cadastrar</button>
+            </form>
+        </field>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
